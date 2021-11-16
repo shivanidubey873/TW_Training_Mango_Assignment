6e1181c4dabe3bc4814b77371be2e51baf709121
shivanidubey873 <67635964+shivanidubey873@users.noreply.github.com>
2021-11-16 18:18:03 +0530

    Mango assignment assigned on 16th November

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.tw;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int row,col,treeNumber;
+        row= sc.nextInt();
+        col= sc.nextInt();
+        treeNumber= sc.nextInt();
+        if((row<=0 && col<=0) || treeNumber<=0){
+            System.out.println("ERROR: There is some error in input");
+        }
+        else{
+            int rowOfGivenTree,colOfGivenTree;
+            rowOfGivenTree=treeNumber/row+1;
+            colOfGivenTree=treeNumber%col;
+            if(rowOfGivenTree==1 || colOfGivenTree==1 || colOfGivenTree==col){
+                System.out.println("yes");
+            }
+            else {
+                System.out.println("no");
+            }
+        }
+    }
+}
